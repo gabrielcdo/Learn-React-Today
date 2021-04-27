1569a596393d97d0221130175165b062edb83018
gabrielcdo <gabrielgc.883@gmail.com>
2021-04-27 12:23:11 -0300

    lesson 7

--- webreact/src/App.js
+++ webreact/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import  React ,  {  useState , useContext}from 'react'
+import Counter from './Counter';
+import CounterHooks from './CounterHooks';
 
+export const ThemeContext = React.createContext
 function App() {
+
+  const [ theme , setTheme ] = useState('red')
   return (
-    <div className="App">
-      <button>-</button>
-      <span>0</span>
-      <button>+</button>
-    </div>
-  );
+    <ThemeContext.Provider value={{
+      backgroundColor: theme
+    }}>
+      Counter
+    <Counter initialCount={0}/>
+     Counter Hooks
+    <CounterHooks initialcount={5}></CounterHooks>
+    <button onClick={prevTheme=>{
+      return prevTheme === 'red' ?  'blue' : 'red'}}>Toogle theme</button>
+    </ThemeContext.Provider>
+  )
 }
 
 export default App;
